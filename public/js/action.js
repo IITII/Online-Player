34d5d74780ad7bcfe0521a514eb27911da1f8940
IITII <ccmejx@gmail.com>
2019-12-05 16:41:33 +0800

    key Listen is finish

--- public/js/action.js
+++ public/js/action.js
@@ -4,13 +4,37 @@ const config = {
     startStopId: "startStop",
     startStopText: ["Start", "Stop"],
     loopId: "loop",
-    loopText: ["Loop: true", "Loop: false"]
+    loopText: ["Loop: true", "Loop: false"],
+    timeAdjust: {
+        forward: 5,
+        backward: 5,
+        increaseVolume: 0.1,
+        decreaseVolume: 0.1,
+    },
+    Rate: {
+        increase: 0.05,
+        decrease: 0.05,
+        default: 1
+    },
+    buttonName: "#copyBtn"
 };
 // toolTip
 $(document).ready(function () {
+    // Load ToolTip
     $(function () {
         $('[data-toggle="tooltip"]').tooltip()
-    })
+    });
+    // Load Clipboard Event
+    new ClipboardJS(config.buttonName);
+    // true for increase volume
+    // false for decrease volume
+    document.addEventListener('mousewheel', function (event) {
+        //console.log( event.wheelDelta > 0 )
+        if (player.isFullscreen() || player.isFullWindow) {
+            event.wheelDelta > 0 ? forwardBackward(38)
+                : forwardBackward(40);
+        }
+    }, false);
 });
 // load player
 window.onload = videojs('onlinePlayer', {
@@ -29,16 +53,31 @@ window.onload = videojs('onlinePlayer', {
                     // L 键控制是否循环播放
                     loopPlayer();
                     break;
+                case 13:
+                    // Enter 控制全屏
+                    fullScreen();
+                    break;
+                case 16:
+                    // Shift 控制浏览器全屏
+                    fullWindow();
+                    break;
                 default:
+                    //方向键监听
+                    forwardBackward(event.which);
+                    //播放速度控制
+                    playRate(event.which);
                     break;
             }
         }
     }
 });
 
+//注册 player
+const player = videojs.getPlayer(config.playerId);
+
 // key functions
 function startStop() {
-    let player = videojs.getPlayer(config.playerId);
+
     //let button = document.getElementById(config.startStopId);
     if (!player.paused()) {
         player.pause();
@@ -50,7 +89,7 @@ function startStop() {
 }
 
 function loopPlayer() {
-    let player = videojs.getPlayer(config.playerId);
+
     if (player.loop()) {
         player.loop(false);
         // document.getElementById(config.loopId).innerText = config.loopText[0];
@@ -61,16 +100,17 @@ function loopPlayer() {
 }
 
 function sourceChange() {
-    let player = videojs.getPlayer(config.playerId);
+
     let src = document.getElementById('directURL').value;
-    if (!(src === ""))
+    if (!(src === "")) {
         player.src(src);
-    else
+        document.getElementById('copyData').value = src;
+    } else
         alert("输入框为空！！！")
 }
 
 function movieChange() {
-    let player = videojs.getPlayer(config.playerId);
+
     let url = document.getElementById('URL').value;
     if (!(url === "")) {
         let Url = new URL(url);
@@ -86,7 +126,82 @@ function movieChange() {
             });
         } else
             alert("Error URL!!!");
-    }else{
+    } else {
         alert("输入框为空！！！");
+    }
+}
+
+function fullScreen() {
+
+    //if (player.supportsFullScreen()) {
+    if (!player.isFullscreen()) {
+        player.requestFullscreen();
+    } else {
+        player.exitFullscreen();
+    }
+    // }else{
+    //     alert("Full Screen is not Support by your Browser! Try to Change Another!")
+    // }
+}
+
+function fullWindow() {
+
+    if (!player.isFullWindow) {
+        player.enterFullWindow();
+    } else {
+        player.exitFullWindow();
+    }
+}
+
+function forwardBackward(keyValue) {
+
+    switch (keyValue) {
+        // 方向键左
+        case 37:
+            player.currentTime(player.currentTime() - config.timeAdjust.backward);
+            console.log("Volume: " + player.currentTime());
+            break;
+        //方向键上
+        case 38:
+            player.volume(player.volume() + config.timeAdjust.increaseVolume);
+            console.log("Volume: " + player.volume());
+            break;
+        //方向键右
+        case 39:
+            player.currentTime(player.currentTime() + config.timeAdjust.forward);
+            console.log("Volume: " + player.currentTime());
+            break;
+        //方向键 下
+        case 40:
+            player.volume(player.volume() - config.timeAdjust.decreaseVolume);
+            console.log("Volume: " + player.volume());
+            break;
+    }
+}
+
+function playRate(keyValue) {
+    switch (keyValue) {
+        case 67:
+            if (player.playbackRate() < 12) {
+                player.playbackRate(player.playbackRate() + config.Rate.increase);
+                console.log("Speed: " + player.playbackRate());
+            } else {
+                alert("视频播放速度不能高于12！！！");
+            }
+            break;
+        case 88:
+            if (player.playbackRate() > 0.1) {
+                player.playbackRate(player.playbackRate() - config.Rate.decrease);
+                console.log("Speed: " + player.playbackRate());
+            } else {
+                alert("视频播放速度不能低于0.1！！！")
+            }
+            break;
+        case 90:
+            player.playbackRate(config.Rate.default);
+            console.log("Speed: " + player.playbackRate());
+            break;
+        default:
+            break;
     }
 }
